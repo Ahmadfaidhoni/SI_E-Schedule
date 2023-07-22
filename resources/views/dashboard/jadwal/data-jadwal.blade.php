f464d5a980eea6ec72ab38b54c1515d23651de16
Sulaiman <sulaiman.saleh1100@gmail.com>
2023-07-22 09:38:18 +0700

    fix : bug permission keuangan

--- resources/views/dashboard/jadwal/data-jadwal.blade.php
+++ resources/views/dashboard/jadwal/data-jadwal.blade.php
@@ -1,133 +1,142 @@
 @extends('layouts.master')
 @section('content')
+    <div class="row">
+        <div class="col-12">
+            <h4 class="">Data Jadwal</h4>
+            <div class="card">
+                <div class="card-body">
 
-<div class="row">
-    <div class="col-12">
-        <h4 class="">Data Jadwal</h4>
-        <div class="card">
-            <div class="card-body">
-                
-                @if(session()->has('success'))
-                <div class="alert alert-success my-3 mx-4 col-lg-8">
-                    {{ session('success') }}
-                </div>
-                @elseif(session()->has('error'))
-                <div class="alert alert-danger my-3 mx-4 col-lg-8">
-                    {{ session('error') }}
-                </div>
-                @endif
+                    {{-- @if (session()->has('success'))
+                        <div class="alert alert-success my-3 mx-4 col-lg-8">
+                            {{ session('success') }}
+                        </div>
+                    @elseif(session()->has('error'))
+                        <div class="alert alert-danger my-3 mx-4 col-lg-8">
+                            {{ session('error') }}
+                        </div>
+                    @endif --}}
 
-                @can('admin')
-                <div class="ml-4" style="display: inline-block">
-                    <a href="/add-jadwal"><button type="button" class="btn btn-primary"><i class="bi bi-calendar-plus"></i> Buat Jadwal</button></a>
-                </div>
-                @endcan
-                <div class="mx-2" style="display: inline-block">
-                    <a href="/history-jadwal"><button type="button" class="btn btn-info"><i class="bi bi-calendar2-week"></i> History</button></a>
-                </div>
-                <div class="mx-2" style="display: inline-block">
-                    {{-- button to trigger modal --}}
-                    <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">
-                       Export
-                    </button>
-                </div>
+                    @can('admin')
+                        <div class="ml-4" style="display: inline-block">
+                            <a href="/add-jadwal"><button type="button" class="btn btn-primary"><i
+                                        class="bi bi-calendar-plus"></i> Buat Jadwal</button></a>
+                        </div>
+                    @endcan
+                    <div class="mx-2" style="display: inline-block">
+                        <a href="/history-jadwal"><button type="button" class="btn btn-info"><i
+                                    class="bi bi-calendar2-week"></i> History</button></a>
+                    </div>
+                    <div class="mx-2" style="display: inline-block">
+                        {{-- button to trigger modal --}}
+                        <button type="button" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">
+                            Export
+                        </button>
+                    </div>
 
-                {{-- make modal --}}
-                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Export PDF</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                {{-- input date range --}}
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text" >Tanggal</span>
-                                    </div>
-                                    <input type="date" id="date-awal" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                    {{-- make modal --}}
+                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Export PDF</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
                                 </div>
-                                <div class="input-group mb-3">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text" >Tanggal</span>
+                                <div class="modal-body">
+                                    {{-- input date range --}}
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Tanggal</span>
+                                        </div>
+                                        <input type="date" id="date-awal" class="form-control"
+                                            aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
                                     </div>
-                                    <input type="date"id="date-akhir"  class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    <div class="input-group mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">Tanggal</span>
+                                        </div>
+                                        <input type="date"id="date-akhir" class="form-control"
+                                            aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                    </div>
+
                                 </div>
-                                
-                            </div>
-                            <div class="modal-footer">
-                                <button onclick="myFunction()" class="btn btn-primary">Export</button>
+                                <div class="modal-footer">
+                                    <button onclick="myFunction()" class="btn btn-primary">Export</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
 
-                
-                <div class="table-responsive">
-                    <table class="table table-striped table-bordered zero-configuration">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Kegiatan</th>
-                                <th>Pegawai</th>
-                                <th>Jumlah JP</th>
-                                <th>Tanggal Kegiatan</th>
-                                <th>Jam</th>
-                                <th>Angkatan</th>
-                                <th>Aksi</th>
-                                
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($jadwal as $jdwl)
-                            <tr>
-                                <td>{{ $loop->iteration }}</td>
-                                <td>
-                                    @if ($jdwl->tipe_jadwal == 2)
-                                        Perjalanan Dinas
-                                    @else
-                                        {{ isset($jdwl->kegiatan)?$jdwl->kegiatan->nama_kegiatan: '-' }}
-                                    @endif
-                                </td>
-                                <td>{{ isset($jdwl->user)?$jdwl->user->name:'-' }}</td>
-                                <td>
-                                    @if($jdwl->jp < 15)
-                                        {{ $jdwl->jp }}
-                                    @else
-                                        Full Day
-                                    @endif
-                                </td>
-                                <td>{{ date('d-m-Y', strtotime($jdwl->waktu_mulai)) }}</td>
-                                
-                                <td>{{ date('H:i', strtotime($jdwl->waktu_mulai)) }} - {{ date('H:i', strtotime($jdwl->waktu_selesai)) }}</td>
-                                <td>{{ isset($jdwl->angkatan)?$jdwl->angkatan:'-' }}</td>
-                                <td>
-                                    @if(Auth::user()->level == 'Admin')
-                                    <div class="row">
-                                        <div class="col-lg-12" style="white-space: nowrap">
-                                            <a href="data-jadwal-{{ $jdwl->id }}"><button type="button" class="btn btn-sm mb-1 btn-primary"><i class="bi bi-eye"></i> Lihat</button></a>
-                                        </div>
-                                    </div>
-                                    @elseif(Auth::user()->level == 'User')
-                                        <a href="{{ $jdwl->id }}.editJadwal"><button type="button" class="btn btn-sm mb-1 btn-danger text-white">Request Ubah Jadwal</button></a>
-                                    @endif
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tfoot>
-                    </table>
+
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered zero-configuration">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Kegiatan</th>
+                                    <th>Pegawai</th>
+                                    <th>Jumlah JP</th>
+                                    <th>Tanggal Kegiatan</th>
+                                    <th>Jam</th>
+                                    <th>Angkatan</th>
+                                    <th>Aksi</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($jadwal as $jdwl)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>
+                                            @if ($jdwl->tipe_jadwal == 2)
+                                                Perjalanan Dinas
+                                            @else
+                                                {{ isset($jdwl->kegiatan) ? $jdwl->kegiatan->nama_kegiatan : '-' }}
+                                            @endif
+                                        </td>
+                                        <td>{{ isset($jdwl->user) ? $jdwl->user->name : '-' }}</td>
+                                        <td>
+                                            @if ($jdwl->jp < 15)
+                                                {{ $jdwl->jp }}
+                                            @else
+                                                Full Day
+                                            @endif
+                                        </td>
+                                        <td>{{ date('d-m-Y', strtotime($jdwl->waktu_mulai)) }}</td>
+
+                                        <td>{{ date('H:i', strtotime($jdwl->waktu_mulai)) }} -
+                                            {{ date('H:i', strtotime($jdwl->waktu_selesai)) }}</td>
+                                        <td>{{ isset($jdwl->angkatan) ? $jdwl->angkatan : '-' }}</td>
+                                        <td>
+                                            @if (Auth::user()->level == 'Admin')
+                                                <div class="row">
+                                                    <div class="col-lg-12" style="white-space: nowrap">
+                                                        <a href="data-jadwal-{{ $jdwl->id }}"><button type="button"
+                                                                class="btn btn-sm mb-1 btn-primary"><i
+                                                                    class="bi bi-eye"></i> Lihat</button></a>
+                                                    </div>
+                                                </div>
+                                            @elseif(Auth::user()->level == 'User' || Auth::user()->level == 'Keuangan')
+                                                <a href="{{ $jdwl->id }}.editJadwal"><button type="button"
+                                                        class="btn btn-sm mb-1 btn-danger text-white">Request Ubah
+                                                        Jadwal</button></a>
+                                            @endif
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tfoot>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 @section('page_script')
     <script>
-        function myFunction(){
+        function myFunction() {
             // get value from date
             var date_awal = document.getElementById('date-awal').value;
             var date_akhir = document.getElementById('date-akhir').value;
