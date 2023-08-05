da96cf510247cb2480a0c45e583a554b74663a33
Ahmad Faidhoni <ahmadfaidhoni03@gmail.com>
2023-08-05 21:02:37 +0700

    fix UI perubahan jadwal

--- resources/views/dashboard/rubah-jadwal/edit-jadwal.blade.php
+++ resources/views/dashboard/rubah-jadwal/edit-jadwal.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div class="row justify-content-center">
     <div class="col-lg-12">
-        <div class="card">
-            <h4 class="card-header">Data Jadwal {{ $jadwal->user->name }}</h4>
+        <h4 class="mx-3">Data Jadwal {{ $jadwal->user->name }}</h4>
+        <div class="card mx-3 mt-3">
             <div class="card-body">
                 <div class="form-validation">
                     <form class="form-valide" action="data-ubah-jadwal.{{ $jadwal->id }}" method="post">
