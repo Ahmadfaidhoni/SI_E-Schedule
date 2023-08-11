361e2dc4f08ae75ab6336e3ceef6e293ec3ac03d
inii-man <manz.iman99@gmail.com>
2023-08-11 11:42:16 +0700

    fix : delete ruangan

--- routes/web.php
+++ routes/web.php
@@ -86,7 +86,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::post('/add-ruangan', [RuanganController::class, 'store']);
     Route::get('editRuangan-{ruangan:id}', [RuanganController::class, 'edit']);
     Route::patch('data-ruangan.{ruangan}', [RuanganController::class, 'update']);
-    Route::delete('data-ruangan.{ruangan}', [RuanganController::class, 'destroy']);
+    Route::delete('data-ruangan/{id}', [RuanganController::class, 'destroy']);
 
     //config
     Route::get('/config', [ConfigController::class, 'index']);
